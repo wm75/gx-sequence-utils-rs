77b349a69b0c1a16d9be59967738bb7c51ed2483
Wolfgang Maier <maierw@informatik.uni-freiburg.de>
2023-04-17 12:27:24 +0200

    Allow builds without warnings

--- src/bin/fastq_to_fasta.rs
+++ src/bin/fastq_to_fasta.rs
@@ -1,17 +1,3 @@
-use std::env;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let input_filename = &args[1];
-    let output_filename = &args[2];
-
-    let num_reads = 0;
-    let fastq_read: Option<String> = None;
-
-    if num_reads == 0 {
-        println!("No valid FASTQ reads could be processed from {input_filename}");
-    } else {
-        println!("{num_reads} FASTQ reads were converted to FASTA.");
-    }
+    println!("Hello, world!");
 }
